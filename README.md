2fedc7a5e28814037effed2813ae27ee6030f16e
wesley <lele.robert18@gmail.com>
2022-11-07 14:44:47 -0300

    comitando a pagína toda para meu GitHub

---
+++ README.md
@@ -0,0 +1,3 @@
+# Trilha JS Developer - Pokedex
+
+Teste de modificação
---
+++ assets/js/poke-api.js
@@ -0,0 +1,39 @@
+const pokeApi ={}
+
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.id
+    pokemon.name = pokeDetail.name
+
+
+    const types =pokeDetail.types.map((typeSlot)=>typeSlot.type.name)
+    const [type] =types
+
+    pokemon.types = types
+    pokemon.type =type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+
+    return pokemon
+}
+
+pokeApi.getPokemonDetails = (pokemon) =>{
+    return fetch(pokemon.url)
+            .then((response)=>response.json())
+            .then(convertPokeApiDetailToPokemon)
+
+}
+
+
+pokeApi.getPokemons = (offset = 370, limit =16) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) =>pokemons.map(pokeApi.getPokemonDetails))
+        .then ((datailRequests)=> Promise.all(datailRequests))
+        .then ((pokemonsDetails) => pokemonsDetails)
+}
+
